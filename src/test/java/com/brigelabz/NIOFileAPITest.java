f6e5de5899c9e4b31dc4b239c9e5b69d109f5fc5
arjunkotari <arjun.kotari123@gmail.com>
2021-08-12 15:22:41 +0530

    Added:Create watch service to watch particulr directory,all files and sub directories

--- src/test/java/com/brigelabz/NIOFileAPITest.java
+++ src/test/java/com/brigelabz/NIOFileAPITest.java
@@ -1,6 +1,7 @@
 package com.brigelabz;
 
 import com.bridgelabz.FileUtils;
+import com.bridgelabz.Java8WatchServiceExample;
 import org.junit.Assert;
 import org.junit.Test;
 import java.io.IOException;
@@ -40,6 +41,12 @@ public class NIOFileAPITest {
         Files.newDirectoryStream(playPath).forEach(System.out::println);
         Files.newDirectoryStream(playPath, path -> path.toFile().isFile() && path.toString().startsWith("temp"))
                 .forEach(System.out::println);
+    }
+    @Test
+    public void givenADirectoryWhenWatchedListsAllTheActivities() throws IOException{
+        Path dir = Paths.get(HOME+ "/"+PLAY_WITH_NIO);
+        Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+        new Java8WatchServiceExample(dir).processEvent();
     }
 }
 
